bf21073c0a13e91ddb4e6f44d2fb0df1ca47c546
Zavrazhin <mr.zavrazhin.p@gmail.com>
2024-01-03 23:04:48 +0300

    fixed

--- Task_3/test_homework_3.py
+++ Task_3/test_homework_3.py
@@ -6,6 +6,6 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
+    response = app.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "We'd like to welcome"}
