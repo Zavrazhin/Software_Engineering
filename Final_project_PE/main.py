ed1b69112e7fe79761e6c5bba154d830e41794af
Zavrazhin <mr.zavrazhin.p@gmail.com>
2024-01-05 15:43:06 +0300

    fix final_project

--- Final_project_PE/main.py
+++ Final_project_PE/main.py
@@ -1,5 +1,28 @@
 import streamlit as st
 import requests
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+
+
+class Item(BaseModel):
+    text: str
+
+
+app = FastAPI()
+pipe = pipeline("summarization", model="IlyaGusev/rut5_base_sum_gazeta")
+
+
+@app.get("/")
+def root():
+    return {"msg": "welcome to our API, we'll help you shorten the text"}
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    """Summarization text"""
+
+    return pipe(item.text)
 
 
 st.title('Суммаризация текста')
