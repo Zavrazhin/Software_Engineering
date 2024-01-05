42de0e5dd401ffab7bbfb56da630f3f2cbfd3609
Zavrazhin <mr.zavrazhin.p@gmail.com>
2024-01-05 12:03:04 +0300

    fixed_26 create python-app.yml

--- Task_3/test_homework_3.py
+++ Task_3/test_homework_3.py
@@ -5,6 +5,6 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = app.get("/")
+    response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "We'd like to welcome"}
